84d65abbbdc07172f825c68d41e177ef86934be2
ErickArambula <eric.alejandro@hotmail.com>
2024-05-15 01:18:40 -0600

    Interfaz grafica 1.0

--- CLUE.java
+++ CLUE.java
@@ -68,6 +68,104 @@ public class CLUE extends JFrame implements ActionListener{
 	add(botonEmpezar);
 	botonEmpezar.setVisible(false);
 
+	botonLugar = new JButton("Lugar");
+	botonLugar.setBounds(340, 180, 100, 25);
+	add(botonLugar);
+	botonLugar.setVisible(false);
+
+	botonArma = new JButton("Arma");
+	botonArma.setBounds(450, 180, 100, 25);
+	add(botonArma);
+	botonArma.setVisible(false);
+
+	botonPersonaje = new JButton("Personaje");
+	botonPersonaje.setBounds(560, 180, 100, 25);
+	add(botonPersonaje);
+	botonPersonaje.setVisible(false);
+
+
+
+//--------------------BOTONES DE PISTAS----------------------
+
+	botonLbarril = new JButton("Barril del chavo");
+	botonLbarril.setBounds(10, 180, 160, 25);
+	add(botonLbarril);
+	botonLbarril.setVisible(false);
+
+	botonLcasaDR = new JButton("Casa de Don Ramon");
+	botonLcasaDR.setBounds(180, 180, 160, 25);
+	add(botonLcasaDR);
+	botonLcasaDR.setVisible(false);
+
+	botonLcasaDF = new JButton("Casa de Dona Florinda");
+	botonLcasaDF.setBounds(350, 180, 160, 25);
+	add(botonLcasaDF);
+	botonLcasaDF.setVisible(false);
+
+	botonLpatio = new JButton("Patio principal");
+	botonLpatio.setBounds(520, 180, 160, 25);
+	add(botonLpatio);
+	botonLpatio.setVisible(false);
+
+	botonLcasaB71 = new JButton("Casa de la Bruja 71");
+	botonLcasaB71.setBounds(690, 180, 160, 25);
+	add(botonLcasaB71);
+	botonLcasaB71.setVisible(false);
+
+//---------------------------------------------------------------
+	botonAchip = new JButton("Chipote Chillon");
+	botonAchip.setBounds(10, 180, 160, 25);
+	add(botonAchip);
+	botonAchip.setVisible(false);
+
+	botonApelota = new JButton("Pelota cuadrada");
+	botonApelota.setBounds(180, 180, 160, 25);
+	add(botonApelota);
+	botonApelota.setVisible(false);
+
+	botonAescoba = new JButton("Escoba voladora");
+	botonAescoba.setBounds(350, 180, 160, 25);
+	add(botonAescoba);
+	botonAescoba.setVisible(false);
+
+	botonAcuchillo = new JButton("Cuchillo que no corta");
+	botonAcuchillo.setBounds(520, 180, 160, 25);
+	add(botonAcuchillo);
+	botonAcuchillo.setVisible(false);
+
+	botonAvalero = new JButton("Valero de madera");
+	botonAvalero.setBounds(690, 180, 160, 25);
+	add(botonAvalero);
+	botonAvalero.setVisible(false);
+
+//-------------------------------------------------------------------
+	botonPchavo = new JButton("El cahvo del 8");
+	botonPchavo.setBounds(10, 180, 160, 25);
+	add(botonPchavo);
+	botonPchavo.setVisible(false);
+
+	botonPflorinda = new JButton("Dona Florinda");
+	botonPflorinda.setBounds(180, 180, 160, 25);
+	add(botonPflorinda);
+	botonPflorinda.setVisible(false);
+
+	botonPramon = new JButton("Don Ramon");
+	botonPramon.setBounds(350, 180, 160, 25);
+	add(botonPramon);
+	botonPramon.setVisible(false);
+
+	botonPquico = new JButton("Quico");
+	botonPquico.setBounds(520, 180, 160, 25);
+	add(botonPquico);
+	botonPquico.setVisible(false);
+
+	botonPchilindrina = new JButton("La chilindrina");
+	botonPchilindrina.setBounds(690, 180, 160, 25);
+	add(botonPchilindrina);
+	botonPchilindrina.setVisible(false);
+
+
+//-----------------------------------------------------------
 
 
 	boton.addActionListener(this);
@@ -76,7 +174,8 @@ public class CLUE extends JFrame implements ActionListener{
 	botonContinuar2.addActionListener(this);
 	botonEmpezar.addActionListener(this);
 
-	/*botonPersonaje.addActionListener(this);
+
+	botonPersonaje.addActionListener(this);
 	botonLugar.addActionListener(this);
 	botonArma.addActionListener(this); 
 
@@ -94,7 +193,8 @@ public class CLUE extends JFrame implements ActionListener{
 	botonPflorinda.addActionListener(this); 
 	botonPramon.addActionListener(this); 
 	botonPquico.addActionListener(this); 
-	botonPchilindrina.addActionListener(this);*/
+	botonPchilindrina.addActionListener(this);
+
 
   }//Fin del constructor 
 //---------------------------------------------------------------------------
@@ -125,6 +225,55 @@ public class CLUE extends JFrame implements ActionListener{
 	  botonEmpezar.setVisible(true);
 	  etiqueta3.setText("Tendras solo 5 oportunidades de obtener informacion antes de elegir al culpable, usalas bien!.");
 	}
+
+	if(accion.getSource() == botonEmpezar){
+	  botonEmpezar.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+	  etiqueta3.setText("Que quieres investigar ??");
+	}
+
+	if(accion.getSource() == botonLugar){
+	  botonLugar.setVisible(false);
+	  botonArma.setVisible(false);
+	  botonPersonaje.setVisible(false);
+
+	  botonLbarril.setVisible(true);
+	  botonLcasaDR.setVisible(true);
+	  botonLcasaDF.setVisible(true);
+	  botonLpatio.setVisible(true);
+	  botonLcasaB71.setVisible(true);
+	  etiqueta3.setText("Que lugar quieres investigar ??");
+	}
+
+	if(accion.getSource() == botonArma){
+	  botonLugar.setVisible(false);
+	  botonArma.setVisible(false);
+	  botonPersonaje.setVisible(false);
+
+	  botonAchip.setVisible(true);
+	  botonApelota.setVisible(true);
+	  botonAescoba.setVisible(true);
+	  botonAcuchillo.setVisible(true);
+	  botonAvalero.setVisible(true);
+	  etiqueta3.setText("Que arma quieres investigar ??");
+	}
+
+	if(accion.getSource() == botonPersonaje){
+	  botonLugar.setVisible(false);
+	  botonArma.setVisible(false);
+	  botonPersonaje.setVisible(false);
+
+	  botonPchavo.setVisible(true);
+	  botonPflorinda.setVisible(true);
+	  botonPramon.setVisible(true);
+	  botonPquico.setVisible(true);
+	  botonPchilindrina.setVisible(true);
+	  etiqueta3.setText("Que arma quieres investigar ??");
+	}
+
+
 
 	
   }//Fin del metodo actionPerformed
