60febc4e8dd0c8ec4647ad282c056d2585730d41
ErickArambula <eric.alejandro@hotmail.com>
2024-05-27 01:12:20 -0600

    Interfaz grafica y motor de inferencia 2.0

--- CLUE.java
+++ CLUE.java
@@ -2,22 +2,68 @@ import javax.swing.*;   //Necesario para diseñar ventanas
 import java.awt.event.*; //Necesario para disparar eventos
 import java.awt.*; //Necesario para manejar color, font e imagen
 
+import java.util.Random;  //Necesario para generar los numeros aleatorios
+
 public class CLUE extends JFrame implements ItemListener, ActionListener{
 
   public JLabel etiquetaIMG, etiqueta1, etiqueta2, etiqueta3, etiquetaIncriminar;
-  public JButton boton, botonSalir, botonContinuar, botonContinuar2, botonEmpezar, botonPersonaje, botonLugar, botonArma, botonLbarril, botonLcasaDR, botonLcasaDF, botonLpatio, botonLcasaB71, botonAchip, botonApelota, botonAescoba, botonAcuchillo, botonAvalero, botonPchavo, botonPflorinda, botonPramon, botonPquico, botonPchilindrina, botonVolver,botonIncriminar;
+  public JButton boton, botonSalir, botonContinuar, botonContinuar2, botonEmpezar, botonPersonaje, botonLugar, botonArma, botonLbarril, botonLcasaDR, botonLcasaDF, botonLpatio, botonLcasaB71, botonAchip, botonApelota, botonAescoba, botonAcuchillo, botonAvalero, botonPchavo, botonPflorinda, botonPramon, botonPquico, botonPchilindrina, botonVolver,botonIncriminar,botonJDN;
   public JComboBox <String> comboLugar, comboPersonaje, comboArma;
-  int asesino = 0;
-  int inocente = 0;
-  int kk;
+  int contIncriminar = 0;
+  public String cadena1, cadena2, cadena3, cadena4, cadena5;
+
+/*
+  int inocente = 0
+  int kk
+*/
 
 //*************************VARIABLES DE TEXTO********************************
 
-  public String chavo, donaFlorinda, donRamon, quico, chilindrina, Barril, casaDR, casaDF, patio, casaB71, chipoteCH, pelotaC, escoba, cuchillo, valero;
+  public String chavoA = "El chavo del 8 estaba manchado de sangre en ";
+  public String chavo = "El chavo del 8 estaba en ";
+
+  public String donaFlorinda = "Dona Florinda estaba preparando una tasita de cafe en ";
+  public String donaFlorindaA = "Dona Florinda se encontraba muy nerviosa en ";
+
+  public String donRamonA = "Don Ramon tenia sangre en su playera en ";
+  public String donRamon = "Don Ramon estaba boxeando en ";
+
+  public String quicoA = "Quico tenia sangre en sus manos y estaba regresando de ";
+  public String quico = "Quico estaba jugando en ";
+
+  public String chilindrinaA = "La chilindrina estaba llorando muy nerviosa en ";
+  public String chilindrina = "La chilindrina estaba llorando en ";
+
+   
+
+  public String barril = "el Barril del chavo usando su ";
+  public String casaDR = "la casa de Don Ramon con su ";
+  public String casaDF = "la casa de Dona Florinda con su ";
+  public String patio = "el patio principal practicando con un ";
+  public String casaB71 = "la casa de la Bruja del 71 con su ";
+
+
+
+  public String chipoteCH = "chipote chillon.";
+  public String pelotaC = "pelota cuadrada.";
+  public String escoba = "escoba voladora.";
+  public String cuchillo = "cuchillo que no corta.";
+  public String valero = "valero de madera.";
 
 //---------------------------------------------------------------------------
 
-//********************************CONSTRUCTOR*******************************
+//**************************GENERANDO NUMEROS ALEATORIOS*********************	
+
+  Random random = new Random();
+
+  int randomNumber1;  //  = random.nextInt(5) + 1;
+  int randomNumber2;  // = random.nextInt(5) + 1;
+  int randomNumber3;
+
+
+//---------------------------------------------------------------------------
+
+//********************************CONSTRUCTOR**************************************************************************************************
   public CLUE (){
 	setLayout(null);
 	setTitle("CLUE del CHAVO");
@@ -34,7 +80,7 @@ public class CLUE extends JFrame implements ItemListener, ActionListener{
 
 	Font fuente = new Font("Broadway", Font.BOLD, 35);
 	etiqueta1 = new JLabel("CLUE del CHAVO !!");
-	etiqueta1.setBounds(20, 20, 600, 30); // x, y, ancho, alto
+	etiqueta1.setBounds(20, 20, 600, 30); // x, y, ancho, alto 
 	etiqueta1.setFont(fuente);
 	etiqueta1.setForeground(Color.WHITE);
 	add(etiqueta1);
@@ -108,6 +154,13 @@ public class CLUE extends JFrame implements ItemListener, ActionListener{
 	botonIncriminar.setBounds(40,510,115,25);
 	add(botonIncriminar);
 	botonIncriminar.setVisible(false);
+
+	botonJDN = new JButton("JUGAR DE NUEVO");
+	botonJDN.setBounds(425, 180, 150, 25);
+	add(botonJDN);
+	botonJDN.setVisible(false);
+
+
 
 
 	comboLugar = new JComboBox<String>();
@@ -246,6 +299,7 @@ public class CLUE extends JFrame implements ItemListener, ActionListener{
 	botonEmpezar.addActionListener(this);
 	botonVolver.addActionListener(this);
 	botonIncriminar.addActionListener(this);
+	botonJDN.addActionListener(this);
 
 
 	botonPersonaje.addActionListener(this);
@@ -274,7 +328,7 @@ public class CLUE extends JFrame implements ItemListener, ActionListener{
 
 
   }//Fin del constructor 
-//---------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 
 //**************************METODO ITEM STATE CHANGED*************************
 
@@ -297,7 +351,7 @@ public class CLUE extends JFrame implements ItemListener, ActionListener{
 
 //----------------------------------------------------------------------------
 
-//**************************METODO ACTION PERFORMED***************************
+//**************************METODO ACTION PERFORMED*****************************************************************************************************
 
   public void actionPerformed(ActionEvent accion){
 
@@ -329,12 +383,26 @@ public class CLUE extends JFrame implements ItemListener, ActionListener{
 	  botonLugar.setVisible(true);
 	  botonArma.setVisible(true);
 	  botonPersonaje.setVisible(true);
-	  botonIncriminar.setVisible(true);
-	  comboLugar.setVisible(true);
-	  comboArma.setVisible(true);
-	  comboPersonaje.setVisible(true);
-	  etiquetaIncriminar.setVisible(true);
+
 	  etiqueta3.setText("Que quieres investigar ??");
+  	  etiqueta2.setVisible(true);
+	  etiqueta2.setText("Han matado al senor barriga!!!");
+
+	  randomNumber1 = random.nextInt(5) + 1;
+	  randomNumber2 = random.nextInt(5) + 1;
+	  randomNumber3 = random.nextInt(5) + 1;
+ 	  contIncriminar = 0;
+
+	  System.out.println(randomNumber1 + "\n");
+	}
+
+	if(accion.getSource() == botonJDN){
+	  botonEmpezar.setVisible(true);
+	  botonJDN.setVisible(false);
+	  //randomNumber1 = random.nextInt(5) + 1;
+	  contIncriminar = 0;
+
+	  System.out.println(randomNumber1 + "\n");
 	}
 
 	if(accion.getSource() == botonLugar){
@@ -406,17 +474,390 @@ public class CLUE extends JFrame implements ItemListener, ActionListener{
 	  etiqueta3.setText("Que quieres investigar ??");
 	}
 
+
+	if(contIncriminar == 5){
+	  comboLugar.setVisible(true);
+	  comboArma.setVisible(true);
+	  comboPersonaje.setVisible(true);
+	  etiquetaIncriminar.setVisible(true);
+	  botonIncriminar.setVisible(true);
+
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(false);
+	  botonArma.setVisible(false);
+	  botonPersonaje.setVisible(false);
+
+
+	  botonLbarril.setVisible(false);
+	  botonLcasaDR.setVisible(false);
+	  botonLcasaDF.setVisible(false);
+	  botonLpatio.setVisible(false);
+	  botonLcasaB71.setVisible(false);
+
+	  botonAchip.setVisible(false);
+	  botonApelota.setVisible(false);
+	  botonAescoba.setVisible(false);
+	  botonAcuchillo.setVisible(false);
+	  botonAvalero.setVisible(false);
+
+	  botonPchavo.setVisible(false);
+	  botonPflorinda.setVisible(false);
+	  botonPramon.setVisible(false);
+	  botonPquico.setVisible(false);
+	  botonPchilindrina.setVisible(false);
+
+	  etiqueta3.setText("Es momento de elegir al culpable!!!!");
+	  etiqueta2.setVisible(false);
+	}
+
+	if(accion.getSource() == botonIncriminar){
+	  comboLugar.setVisible(false);
+	  comboArma.setVisible(false);
+	  comboPersonaje.setVisible(false);
+	  etiquetaIncriminar.setVisible(false);
+	  botonIncriminar.setVisible(false);
+	  botonJDN.setVisible(true);
+	  etiqueta3.setText("//////");
+	}
+
+	
+//******************BOTONES L A P **************************
+
+	if(accion.getSource() == botonLbarril){
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonLbarril.setVisible(false);
+	  botonLcasaDR.setVisible(false);
+	  botonLcasaDF.setVisible(false);
+	  botonLpatio.setVisible(false);
+	  botonLcasaB71.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+
+	  contIncriminar = contIncriminar + 1;
+	}
+
+	if(accion.getSource() == botonLcasaDR){
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonLbarril.setVisible(false);
+	  botonLcasaDR.setVisible(false);
+	  botonLcasaDF.setVisible(false);
+	  botonLpatio.setVisible(false);
+	  botonLcasaB71.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+
+	  contIncriminar = contIncriminar + 1;
+	}
+
+	if(accion.getSource() == botonLcasaDF){
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonLbarril.setVisible(false);
+	  botonLcasaDR.setVisible(false);
+	  botonLcasaDF.setVisible(false);
+	  botonLpatio.setVisible(false);
+	  botonLcasaB71.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+
+	  contIncriminar = contIncriminar + 1;
+	}
+
+	if(accion.getSource() == botonLpatio){
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonLbarril.setVisible(false);
+	  botonLcasaDR.setVisible(false);
+	  botonLcasaDF.setVisible(false);
+	  botonLpatio.setVisible(false);
+	  botonLcasaB71.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+
+	  contIncriminar = contIncriminar + 1;
+	}
+
 	if(accion.getSource() == botonLcasaB71){
-	  asesino = 1;
-	  System.out.println(asesino);
-	  System.out.println(inocente);
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonLbarril.setVisible(false);
+	  botonLcasaDR.setVisible(false);
+	  botonLcasaDF.setVisible(false);
+	  botonLpatio.setVisible(false);
+	  botonLcasaB71.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+
+	  contIncriminar = contIncriminar + 1;
 	}
 
 
 
+
+	if(accion.getSource() == botonAchip){
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonAchip.setVisible(false);
+	  botonApelota.setVisible(false);
+	  botonAescoba.setVisible(false);
+	  botonAcuchillo.setVisible(false);
+	  botonAvalero.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+
+	  contIncriminar = contIncriminar + 1;
+	}
+
+	if(accion.getSource() == botonApelota){
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonAchip.setVisible(false);
+	  botonApelota.setVisible(false);
+	  botonAescoba.setVisible(false);
+	  botonAcuchillo.setVisible(false);
+	  botonAvalero.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+
+	  contIncriminar = contIncriminar + 1;
+	}
+
+	if(accion.getSource() == botonAescoba){
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonAchip.setVisible(false);
+	  botonApelota.setVisible(false);
+	  botonAescoba.setVisible(false);
+	  botonAcuchillo.setVisible(false);
+	  botonAvalero.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+
+	  contIncriminar = contIncriminar + 1;
+	}
+
+	if(accion.getSource() == botonAcuchillo){
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonAchip.setVisible(false);
+	  botonApelota.setVisible(false);
+	  botonAescoba.setVisible(false);
+	  botonAcuchillo.setVisible(false);
+	  botonAvalero.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+
+	  contIncriminar = contIncriminar + 1;
+	}
+
+	if(accion.getSource() == botonAvalero){
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonAchip.setVisible(false);
+	  botonApelota.setVisible(false);
+	  botonAescoba.setVisible(false);
+	  botonAcuchillo.setVisible(false);
+	  botonAvalero.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+
+		if(randomNumber1 == 1){
+		  etiqueta2.setText(cadena5);
+		}
+		if(randomNumber1 == 2){
+		  etiqueta2.setText(cadena1);
+		}
+		if(randomNumber1 == 3){
+		  etiqueta2.setText(cadena2);
+		}
+		if(randomNumber1 == 4){
+		  etiqueta2.setText(cadena3);
+		}
+		if(randomNumber1 == 5){
+		  etiqueta2.setText(cadena4);
+		}
+
+	  System.out.println(randomNumber1);
+
+	  contIncriminar = contIncriminar + 1;
+	}
+
+
+
+
+	if(accion.getSource() == botonPchavo){
+  
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonPchavo.setVisible(false);
+	  botonPflorinda.setVisible(false);
+	  botonPramon.setVisible(false);
+	  botonPquico.setVisible(false);
+	  botonPchilindrina.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+	  etiqueta2.setText(cadena1);
+
+	  contIncriminar = contIncriminar + 1;
+
+	  System.out.println(randomNumber1);
+	}
+
+	if(accion.getSource() == botonPflorinda){
+
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonPchavo.setVisible(false);
+	  botonPflorinda.setVisible(false);
+	  botonPramon.setVisible(false);
+	  botonPquico.setVisible(false);
+	  botonPchilindrina.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+	  etiqueta2.setText(cadena2);
+
+	  contIncriminar = contIncriminar + 1;
+
+	  System.out.println(randomNumber1);
+	}
+
+	if(accion.getSource() == botonPramon){
+
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonPchavo.setVisible(false);
+	  botonPflorinda.setVisible(false);
+	  botonPramon.setVisible(false);
+	  botonPquico.setVisible(false);
+	  botonPchilindrina.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+	  etiqueta2.setText(cadena3);
+
+	  contIncriminar = contIncriminar + 1;
+
+	  System.out.println(randomNumber1);
+	}
+
+	if(accion.getSource() == botonPquico){
+
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonPchavo.setVisible(false);
+	  botonPflorinda.setVisible(false);
+	  botonPramon.setVisible(false);
+	  botonPquico.setVisible(false);
+	  botonPchilindrina.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+	  etiqueta2.setText(cadena4);
+
+	  contIncriminar = contIncriminar + 1;
+
+	  System.out.println(randomNumber1);
+	}
+
+	if(accion.getSource() == botonPchilindrina){
+
+	  botonVolver.setVisible(false);
+	  botonLugar.setVisible(true);
+	  botonArma.setVisible(true);
+	  botonPersonaje.setVisible(true);
+
+	  botonPchavo.setVisible(false);
+	  botonPflorinda.setVisible(false);
+	  botonPramon.setVisible(false);
+	  botonPquico.setVisible(false);
+	  botonPchilindrina.setVisible(false);
+	  etiqueta3.setText("Que mas quieres investigar ??");
+	  etiqueta2.setText(cadena5);
+
+	  contIncriminar = contIncriminar + 1;
+
+	  System.out.println(randomNumber1);
+	}
+
+//---------------------------------------------------------
+
+//__________________________________MOTOR DE INFERENCIA__________________________________________
+
+ 	if(randomNumber1 == 1){
+ 	  cadena1 = chavoA + barril + chipoteCH;
+	  cadena2 = donaFlorinda + casaDR + pelotaC;
+	  cadena3 = donRamon + casaDF + escoba;
+	  cadena4 = quico + patio + cuchillo;
+	  cadena5 = chilindrina + casaB71 + valero;
+  	}
+
+ 	if(randomNumber1 == 2){
+ 	  cadena1 = chavo + casaB71 + valero;
+	  cadena2 = donaFlorindaA + barril + chipoteCH;
+	  cadena3 = donRamon + casaDR + pelotaC;
+	  cadena4 = quico + casaDF + escoba;
+	  cadena5 = chilindrina + patio + cuchillo;
+  	}
+
+ 	if(randomNumber1 == 3){
+ 	  cadena1 = chavo + patio + cuchillo;
+	  cadena2 = donaFlorinda + casaB71 + valero;
+	  cadena3 = donRamonA + barril + chipoteCH;
+	  cadena4 = quico + casaDR + pelotaC;
+	  cadena5 = chilindrina + casaDF + escoba;
+  	}
+
+ 	if(randomNumber1 == 4){
+ 	  cadena1 = chavo + casaDF + escoba;
+	  cadena2 = donaFlorinda + patio + cuchillo;
+	  cadena3 = donRamon + casaB71 + valero;
+	  cadena4 = quicoA + barril + chipoteCH;
+	  cadena5 = chilindrina + casaDR + pelotaC;
+  	}
+
+ 	if(randomNumber1 == 5){
+ 	  cadena1 = chavo + casaDR + pelotaC;
+	  cadena2 = donaFlorinda + casaDF + escoba;
+	  cadena3 = donRamon + patio + cuchillo;
+	  cadena4 = quico + casaB71 + valero;
+	  cadena5 = chilindrinaA + barril + chipoteCH;
+  	}
+	
+
+//________________________________________________________________________________________________
+
+
 	
   }//Fin del metodo actionPerformed
-//----------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //****************************************MAIN********************************
   public static void main(String args[]){
